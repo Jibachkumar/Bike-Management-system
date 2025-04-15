4def270b1d18d0044de8ce65af089a5777e580a9
jibachh <subhamchaudhary199@gmail.com>
2025-04-16 00:04:25 +0545

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="mt-2">
+        <p className="text-2xl">Bike Management system</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
